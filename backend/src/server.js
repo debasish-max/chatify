e43f12761ce07b0c721e71c402ce6567adcec1a4
debasish-max <hazarikadebasish182@gmail.com>
2026-01-29 17:02:36 +0530

    signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,35 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import { connectDB } from './lib/db.js';
-import { ENV } from './lib/env.js';
-
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json()); //middleware to parse json body (req.body)
+app.use(express.json()); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-//make ready for deployment
-if(ENV.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-  })
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ` + PORT)
-  connectDB()
+  console.log("Server running on port: " + PORT);
+  connectDB();
 });
